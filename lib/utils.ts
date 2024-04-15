af5362c594b9b530e9c6de48a10ae146310fbbaf
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-04-15 16:51:05 +0700

    chore: add weather status to icon

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function weatherToIcon(weather: string) {
+  switch (weather) {
+    case "Clear":
+      return "☀️";
+    case "Clouds":
+      return "☁️";
+    case "Rain":
+      return "🌧";
+    case "Snow":
+      return "❄️";
+    default:
+      return "🌈";
+  }
 }
